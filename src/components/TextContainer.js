350ea9ce95a9beee03c0702e41613e700c310f60
Cook1619 <cook6348@gmail.com>
2018-04-12 06:54:37 -0500

    Debugged mini-react-project

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -6,24 +6,27 @@ export default class TextContainer extends Component {
     this.state = {
       text: ''
     }
+    this.updateText = this.updateText.bind(this)
   }
 
-  updateText(e) {
+  updateText(element) {
     this.setState({
-      text: e.target.value
+      text: element.target.value
     })
   }
 
   render() {
+    console.log(this.props.fontColor,this.props.fontFamily,this.props.fontSize)
     return (
       <div className="textContainer">
-        <textarea 
-          // style={  }
+        <textarea
+          style={{ color: this.props.fontColor, fontFamily: this.props.fontFamily, fontSize: this.props.fontSize }}
           onChange={this.updateText}
-          value={this.state.text} 
-          placeholder='Start typing your thoughts here!'
+          value={this.state.text}
+          placeholder='This is where your text will be!'
           cols="90"
           rows="30">
+          
         </textarea>
       </div>
     )
